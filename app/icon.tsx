3780c4adb92cd4707ae7b7b379c0452a6d4ddc46
samuelnoroes <samuel.noroes@gmail.com>
2026-05-14 19:28:04 -0300

    Update icon.tsx

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,1 +1,26 @@
+import { ImageResponse } from "next/og";
 
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    <div
+      style={{
+        background: "#F5F1EA",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 22,
+        fontStyle: "italic",
+        fontFamily: "Georgia, serif",
+        color: "#2D4A3E",
+        fontWeight: 400,
+      }}
+    >
+      M
+    </div>
+  );
+}
